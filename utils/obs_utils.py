6e6f8a871696635b24f22df2bcb10a7bec7e32a3
alianlbj23 <alianlbj23@gmail.com>
2024-02-25 19:24:48 +0800

    給RL, rule的state改成回傳一組data
    原本是為了RL去分成給神經網路看的observation和給reward計算的observation,現在改成到看一個observation,
    不用去區分給network看的和reward計算的

--- utils/obs_utils.py
+++ utils/obs_utils.py
@@ -9,7 +9,7 @@ def data_dict_pop(data_dict):
     data_dict.pop('target_pos', None)
     return data_dict
 
-def process_data(unity_data):  
+def process_data(unity_data):
     '''data轉成np後給env的observation'''
     flat_list = []
     for value in unity_data.values():
@@ -19,12 +19,9 @@ def process_data(unity_data):
             flat_list.append(value)
     return np.array(flat_list, dtype=np.float32)
 
-def wait_for_data(AI_node): 
+def wait_for_data(AI_node):
     '''等待最新的data, 並回傳給reward計算用的state和給obs的state'''
     unity_data = AI_node.get_latest_data()
-    
     while unity_data is None:
         unity_data = AI_node.get_latest_data()
-    unity_data_for_reward = unity_data.copy()
-    unity_data = data_dict_pop(unity_data)
-    return unity_data, unity_data_for_reward
+    return unity_data
